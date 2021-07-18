7d4b7b121548758f64d37061dc03670c6df362d1
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2021-07-18 14:44:23 +0300

    style page1

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -31,16 +31,99 @@ function MainPage(props) {
 
 
     return (
-        <div className='mainPage'>
 
-            <Link to="/statistics">STATISTICS</Link>
-            <h1>Main Page</h1>
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Accusamus at cumque distinctio dolor dolore enim esse fuga harum
-                illum ipsum nisi, quasi qui, quidem quisquam quod sed sit tempora vitae?
+            <div className="mainPage">
+                <header className="header">
+                    <div className="header__content">
+                        <div className="content__title">AppCo</div>
+                        <div className="content__title_1"><span>Brainstorming&nbsp;</span>for<br/>
+                            desired perfect Usability
+                        </div>
+                        <div className="content__text">
+                            Our design projects are fresh and simple and will benefit<br/>
+                            your business greatly. Learn more about our work!
+                        </div>
+                        <div className="content__btn-block">
+                            <Link to="/statistics" className="content__btn">Views Stats</Link>
+                        </div>
+                    </div>
+                    <div className="header__img">
+                        <img src={require("../pages/img/mobile.png")} alt="mobile" className="header__mobile"/>
+                    </div>
+                </header>
+                <main className="page">
+                    <div className="page__title">Why <span>small business owners</span><br/>
+                        <span>love</span> AppCo?
+                    </div>
+                    <div className="page__text">Our design projects are fresh and simple and
+                        will benefit your business <br/>
+                            greatly. Learn more about our work!
+                    </div>
+
+                    <div className="page__block-card">
+
+                        <div className="page__card">
+                            <div className="page__card-container">
+                                <img className="page__card-img" src={require("../pages/img/group1.png")} alt="img"/>
+                                    <div className="page__card-title">Clean Design</div>
+                                    <div className="page__card-text">Increase sales by showing true<br/>
+                                        dynamics of your website.
+                                    </div>
+                            </div>
+
+                        </div>
+
+                        <div className="page__card">
+                            <div className="page__card-container">
+                                <img className="page__card-img" src={require("../pages/img/group2.png")} alt="img"/>
+                                    <div className="page__card-title">Secure Data</div>
+                                    <div className="page__card-text">Build your online store’s trust<br/>
+                                        using Social Proof & Urgency.
+                                    </div>
+                            </div>
+
+
+                        </div>
+                        <div className="page__card">
+                            <div className="page__card-container">
+                                <img className="page__card-img" src={require('../pages/img/group3.png')} alt="img"/>
+                                    <div className="page__card-title">Retina Ready</div>
+                                    <div className="page__card-text">Realize importance of social proof<br/>
+                                        in customer’s purchase decision.
+                                    </div>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </main>
+                <footer className="footer">
+
+                    <div className="footer__container">
+                        <div className="footer__input">
+
+                            <div className="footer__search">
+                                <input type="text" placeholder="Enter your email"/>
+                                    <button>Subscribe</button>
+                            </div>
+
+                        </div>
+
+                        <div className="footer__bottom">
+                            <div className="footer__bottom-right">AppCo</div>
+                            <div className="footer__bottom-center">All rights reserved by ThemeTags</div>
+                            <div className="footer__bottom-left">Copyrights © 2019.</div>
+                        </div>
+
+                    </div>
+
+
+                </footer>
+
             </div>
 
-        </div>
+
     );
 }
 
